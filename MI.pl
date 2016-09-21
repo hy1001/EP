3d0fe7aa2503d4e4b5f8696b56ea4eb92cd88b18
Henry P <hy1001@yahoo.com>
2016-09-20 20:06:48 -0400

    MI.pl updated

--- MI.pl
+++ MI.pl
@@ -9,58 +9,65 @@ foreach my $file_name (@ARGV) {
 
     my @bolds = $tree->find_by_tag_name('b');
 
-    # foreach my $node ( @bolds ) {
-    # 	my @kids = $node->content_list();
-    # 	print @kids, "\n";
-    # 	# print $kids[0]->as_text(), "\n";
-    # }
+    foreach my $node ( @bolds ) {
+    	my @kids = $node->content_list();
+    	print @kids, "\n";
+    	# print $kids[0]->as_text(), "\n";
+    }
 
     print "**", $bolds[0]->content_list(), "\n";
 
-    # my @spans = $tree->look_down( _tag => 'span', lang => 'en-US');
+    my @spans = $tree->look_down( _tag => 'span', lang => 'en-US');
     # print scalar( @spans ), "\n";
     
-    # foreach my $node ( @spans ) {
-    # 	print $node, "\n";
-    # 	my @kids = $node->content_list();
-    # 	print @kids, "\n";
-    # }
+    foreach my $node ( @spans ) {
+    	print $node, "\n";
+    	my @kids = $node->content_list();
+    	print @kids, "\n";
+    }
 
     # print "** ", $spans[2]->content_list(), "\n"; ## Q2_Date
     # print "** ", $spans[3]->content_list(), "\n"; ## Q2_Time
     # print "** ", $spans[9]->content_list(), "\n"; ## Comments
 
     
-    my @DeathCheckBoxes = ("Q1_No", "Q1_Yes", 
-			   "Q3_Cardio", "Q3_NonCV", "Q3_Unk",
-			   "Q4_1", "Q4_3", "Q4_5", "Q4_6", "Q4_8", "Q4_9", 
-			   "Q4_13", "Q4_13a", "Q4_13b", "Q4_13c",
-			   "Q4_14", "Q4_15", "Q4_16", "Q4_17",
-			   "Q5_2", "Q5_4", "Q5_5", "Q5_6", "Q5_7", "Q5_8",
-			   "Q5_9", "Q5_10", "Q5_11", "Q5_12",
-			   "Q6_No", "Q6_Yes" );
+    my @MICheckBoxes = ("Q1_No", "Q1_Yes", "Q2_No", "Q2_Yes", 
+			   "Q4_1", "Q4_2", "Q4_3", "Q4_6", "Q4_5", "Q4_6", 
+			   "Q4_7", 
+			   "Q5_ST", "Q5_NonST", "Q5_Unk",
+			   "Q6_PCI_No", "Q6_PCI_Yes",
+			   "Q6_CABG_No", "Q6_CABG_Yes",
+			   "Q7_No", "Q7_Yes", "Q7_Unk",
+			   "Q8_1_No", "Q8_1_Yes",
+			   "Q8_2_No", "Q8_2_Yes",
+			   "Q8_3_No", "Q8_3_Yes",
+			   "Q8_4_No", "Q8_4_Yes",
+			   "Q8_5_No", "Q8_5_Yes",
+			   "Q8_6_No", "Q8_6_Yes",
+			   "Q8_7_No", "Q8_7_Yes", "Q8_7_Neg", "Q8_7_Pos" );
 
 
     my @checkboxes = $tree->look_down( _tag => 'input', type => 'checkbox' );
     # print scalar( @checkboxes ), "\n";
 
-    # my $i = 0;
-    # foreach my $node ( @checkboxes ) {
-    # 	# print $node, "\n";
-    # 	# print $node->parent()->content_list(), "\n";
-    # 	print $DeathCheckBoxes[ $i++ ], "\n";
+    my $i = 0;
+    foreach my $node ( @checkboxes ) {
+    	# print $node, "\n";
+    	# print $node->parent()->content_list(), "\n";
+    	print $MICheckBoxes[ $i++ ], "\n";
+	## print  $i++ , "\n";
 
-    # 	if ( exists $node->{'checked'} ) {
-    # 	    print "checked\t";
-    # 	} else {
-    # 	    print "\t";
-    # 	}
+    	if ( exists $node->{'checked'} ) {
+    	    print "checked\t";
+    	} else {
+    	    print "\t";
+    	}
 
-    # 	# foreach my $key ( sort keys %{ $node } ) {
-    # 	#     print $key, " : ",  $node->{$key}, "\n";
-    # 	# }
-    # 	print "\n";
-    # }
+    	# foreach my $key ( sort keys %{ $node } ) {
+    	#     print $key, " : ",  $node->{$key}, "\n";
+    	# }
+    	print "\n";
+    }
 
 
     # my @checked = $tree->look_down( 'checked', 'checked' );
