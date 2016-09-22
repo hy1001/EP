f1b148e196f6df45757979322f458a670d80fab2
Henry <hpaik@stark.dpm.bwh.harvard.edu>
2016-09-22 07:52:33 -0400

    hptest4.pl updated traverse.pl added

--- hptest4.pl
+++ hptest4.pl
@@ -1,6 +1,36 @@
 use HTML::TreeBuilder 5 -weak; # Ensure weak references in use
 use Data::Dumper;
 
+sub death {
+    my $tree = shift;
+    my $fileName = shift;
+    print "\nDeath\n";
+    my @spans = $tree->look_down( _tag => 'span', lang => 'en-US');
+
+    # print "\n *** # spans : ", scalar( @spans ), " *** \n";
+    if( scalar( @span ) != 14 ) {
+	print "##############\n";
+	print "\n *** # spans : ", scalar( @spans ), " *** \n";
+    }
+    return 1;
+}
+
+sub mi {
+
+    my $tree  = shift;
+    my $fileName = shift;
+    
+ #   print Dumper( $tree ); 
+    print "\nMI\n";
+    my @spans = $tree->look_down( _tag => 'span', lang => 'en-US');
+    # print "\n *** # spans : ", scalar( @spans ), " *** \n";
+    if( scalar( @spans ) != 23 ) {
+	print "##############\n";
+	print "\n *** # spans : ", scalar( @spans ), " *** \n";
+    }
+    return 1;
+}
+
 foreach my $file_name (@ARGV) {
     my $tree = HTML::TreeBuilder->new; # empty tree
     $tree->parse_file($file_name);
@@ -15,58 +45,64 @@ foreach my $file_name (@ARGV) {
     # 	# print $kids[0]->as_text(), "\n";
     # }
 
-    print "**", $bolds[0]->content_list(), "\n";
+    my @tmp = $bolds[0]->content_list();
+    print "\n******* ",$tmp[0], " ************\n";
+    
+    if ( $tmp[0] =~ m/Death/ ) {
+	&death( $tree , $file_name);
+    }
+    elsif ( $tmp[0]  =~ "Myocardial") {
+     	&mi( $tree, $file_name );
+    }
 
     # my @spans = $tree->look_down( _tag => 'span', lang => 'en-US');
-    # print scalar( @spans ), "\n";
+    # print "\n *** # spans : ", scalar( @spans ), " *** \n";
     
-    # foreach my $node ( @spans ) {
-    # 	print $node, "\n";
-    # 	my @kids = $node->content_list();
-    # 	print @kids, "\n";
-    # }
+    # # foreach my $node ( @spans ) {
+    # # 	print $node, "\n";
+    # # 	my @kids = $node->content_list();
+    # # 	print @kids, "\n";
+    # # }
 
-    # print "** ", $spans[2]->content_list(), "\n"; ## Q2_Date
-    # print "** ", $spans[3]->content_list(), "\n"; ## Q2_Time
-    # print "** ", $spans[9]->content_list(), "\n"; ## Comments
+    # # print "** ", $spans[2]->content_list(), "\n"; ## Q2_Date
+    # # print "** ", $spans[3]->content_list(), "\n"; ## Q2_Time
+    # # print "** ", $spans[9]->content_list(), "\n"; ## Comments
 
     
-    my @DeathCheckBoxes = ("Q1_No", "Q1_Yes", 
-			   "Q3_Cardio", "Q3_NonCV", "Q3_Unk",
-			   "Q4_1", "Q4_3", "Q4_5", "Q4_6", "Q4_8", "Q4_9", 
-			   "Q4_13", "Q4_13a", "Q4_13b", "Q4_13c",
-			   "Q4_14", "Q4_15", "Q4_16", "Q4_17",
-			   "Q5_2", "Q5_4", "Q5_5", "Q5_6", "Q5_7", "Q5_8",
-			   "Q5_9", "Q5_10", "Q5_11", "Q5_12",
-			   "Q6_No", "Q6_Yes" );
+    # my @DeathCheckBoxes = ("Q1_No", "Q1_Yes", 
+    # 			   "Q3_Cardio", "Q3_NonCV", "Q3_Unk",
+    # 			   "Q4_1", "Q4_3", "Q4_5", "Q4_6", "Q4_8", "Q4_9", 
+    # 			   "Q4_13", "Q4_13a", "Q4_13b", "Q4_13c",
+    # 			   "Q4_14", "Q4_15", "Q4_16", "Q4_17",
+    # 			   "Q5_2", "Q5_4", "Q5_5", "Q5_6", "Q5_7", "Q5_8",
+    # 			   "Q5_9", "Q5_10", "Q5_11", "Q5_12",
+    # 			   "Q6_No", "Q6_Yes" );
 
 
-    my @checkboxes = $tree->look_down( _tag => 'input', type => 'checkbox' );
-    # print scalar( @checkboxes ), "\n";
+    # my @checkboxes = $tree->look_down( _tag => 'input', type => 'checkbox' );
+    # print "*** # checkboxes : ", scalar( @checkboxes ), " ***\n";
 
-    # my $i = 0;
-    # foreach my $node ( @checkboxes ) {
-    # 	# print $node, "\n";
-    # 	# print $node->parent()->content_list(), "\n";
-    # 	print $DeathCheckBoxes[ $i++ ], "\n";
+    # # my $i = 0;
+    # # foreach my $node ( @checkboxes ) {
+    # # 	# print $node, "\n";
+    # # 	# print $node->parent()->content_list(), "\n";
+    # # 	print $DeathCheckBoxes[ $i++ ], "\n";
 
-    # 	if ( exists $node->{'checked'} ) {
-    # 	    print "checked\t";
-    # 	} else {
-    # 	    print "\t";
-    # 	}
+    # # 	if ( exists $node->{'checked'} ) {
+    # # 	    print "checked\t";
+    # # 	} else {
+    # # 	    print "\t";
+    # # 	}
 
-    # 	# foreach my $key ( sort keys %{ $node } ) {
-    # 	#     print $key, " : ",  $node->{$key}, "\n";
-    # 	# }
-    # 	print "\n";
-    # }
-
-
-    # my @checked = $tree->look_down( 'checked', 'checked' );
-    my @checked = $tree->look_down( _tag => 'input', checked => 'checked' );
-
-    print scalar( @checked ), "\n";
+    # # 	# foreach my $key ( sort keys %{ $node } ) {
+    # # 	#     print $key, " : ",  $node->{$key}, "\n";
+    # # 	# }
+    # # 	print "\n";
+    # # }
 
 
+    # # my @checked = $tree->look_down( 'checked', 'checked' );
+    # my @checked = $tree->look_down( _tag => 'input', checked => 'checked' );
+
+    # print scalar( @checked ), "\n";
 }
---
+++ traverse.pl
@@ -0,0 +1,104 @@
+use strict;
+use warnings;
+use File::Basename;
+use File::Copy qw(copy);
+
+my $i = 0;
+##my $base_path = "./Previous Cases";
+# my $base_path = "/media/cantosce/Aruna Pradhan";
+# my $base_path = "/media/cantosce/Ben Olenchock";
+
+process_files ($base_path);
+
+# Accepts one argument: the full path to a directory.
+# Returns: nothing.
+sub process_files {
+    my $path = shift;
+
+    # Open the directory.
+    opendir (DIR, $path)
+	or die "Unable to open $path: $!";
+
+    # Read in the files.
+    # You will not generally want to process the '.' and '..' files,
+    # so we will use grep() to take them out.
+    # See any basic Unix filesystem tutorial for an explanation of them
+
+    my @files = grep { !/^\.{1,2}$/ } readdir (DIR);
+    
+    # Close the directory.
+    closedir (DIR);
+
+    # At this point you will have a list of filenames
+    #  without full paths ('filename' rather than
+    #  '/home/count0/filename', for example)
+    # You will probably have a much easier time if you make
+    #  sure all of these files include the full path,
+    #  so here we will use map() to tack it on.
+    #  (note that this could also be chained with the grep
+    #   mentioned above, during the readdir() ).
+    @files = map { $path . '/' . $_ } @files;
+
+    for (@files) {
+
+	my $wholePath = $_;
+	# print $wholePath, "\n";
+	my $filename = basename( $_ );
+	# print $filename, "\n";
+	
+	# If the file is a directory
+	if (-d $_) {
+	    # Here is where we recurse.
+	    # This makes a new call to process_files()
+	    # using a new directory we just found.
+	    process_files ($_);
+
+	    # If it isn't a directory, lets just do some
+	    # processing on it.
+	} elsif  ( $filename =~ /^\w+.*\Q.doc\E$/ )  {
+	## } elsif  ( $_ =~ /^\w{1,}*\.docx$/ )  {
+	    print "\n", $i++, "\n";
+	    print $filename, "\n";
+	    # Do whatever you want here =)
+	    # A common example might be to rename the file.
+	    # print $_, "\n";
+
+	    ### copy the doc file to /tmp/tmp.doc file
+
+	    # copy $_, "/tmp/tmp.doc";
+
+	    # ### convert tmp.doc file to tmp.html file via headless soffice
+	    # system( "soffice --headless --convert-to html --outdir /tmp /tmp/tmp.doc" );
+	    # if ( $? == -1 )
+	    # {
+	    # 	print "command failed: $!\n";
+	    # }
+	    # else
+	    # {
+	    # 	printf "command exited with value %d", $? >> 8;
+	    # }
+
+	    # ### cleaning the characters \a\b\r
+	    # system( " tr -d '\a\b\r' < /tmp/tmp.html > /tmp/tmp2.html " ) ;
+
+	    # ### now work with /tmp/tmp2.html file via perl
+	    # system( " perl /home/hpaik/projects/CANTOS/EndPointCommittee/hptest4.pl /tmp/tmp2.html " );
+	    # if ( $? == -1 )
+	    # {
+	    # 	print "command failed: $!\n";
+	    # }
+	    # else
+	    # {
+	    # 	printf "command exited with value %d", $? >> 8;
+	    # }
+
+	    # ### cleaning up
+	    # unlink "/tmp/tmp.doc";
+	    # unlink "/tmp/tmp.html";
+	    # ### save the html file with the original filename
+	    # rename "/tmp/tmp2.html", "/tmp/".$filename.".html";
+	} else {
+
+	}
+    }
+}
